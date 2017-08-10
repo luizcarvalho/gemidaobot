002fc377eebe6328bbbf767e97323b65e8fbdcdb
Luiz Carvalho <maximusmano@gmail.com>
2017-08-10 19:38:54 -0300

    add gemidao-do-zap code

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var _gemidao = require('./gemidao');
+var gemidao = _interopRequireDefault(_gemidao.default);
 
-app.set('port', (process.env.PORT || 5000));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
+
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -13,13 +22,29 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/bot', function(request, response) {
-  execute('./node_modules/.bin/gemidao-do-zap --version')
+app.post('/bot', function(request, response) {
+  var token = request.body.token
+  var de = request.body.de
+  var para = request.body.para
+  console.log(JSON.stringify(request.body))
+
+  (0, gemidao.default)(request.body).then(function(){
+    console.log('ok');
+  }).catch(function(error) {
+    console.log(error);
+  })
+
   response.setHeader('Content-Type', 'application/json');
-  response.send(JSON.stringify({ a: 1 }));
+  response.send(JSON.stringify(
+    {
+     "messages": [
+       {"text": 'ok'}
+      ]
+    }
+  ));
+
 
 });
-
 
 
 app.listen(app.get('port'), function() {
@@ -27,16 +52,21 @@ app.listen(app.get('port'), function() {
 });
 
 
-function execute(command) {
-  const { exec } = require('child_process');
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
+function execute(cmd, callback) {
+  var spawn = require('child_process').spawn;
+  var command = spawn(cmd);
+  var result = '';
 
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`);
-    });
+  command.stdout.on('data', function(data) {
+     result += data.toString();
+   });
+
+  command.on('close', function(code) {
+    return callback(result);
+  });
+
+  command.on('error', function (err) {
+    console.log('error', err);
+  });
 
 }
