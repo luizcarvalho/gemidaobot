9e37c8e049593bdbf26e29d4b92d7056bf9da339
Luiz Carvalho <maximusmano@gmail.com>
2017-08-10 18:28:30 -0300

    executing binary

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/bot', function(request, response) {
+  execute('./node_modules/.bin/gemidao-do-zap --version')
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ a: 1 }));
+
+});
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+function execute(command) {
+  const { exec } = require('child_process');
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+    });
+
+}
